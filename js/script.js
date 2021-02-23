be1717febff31eb100353396a74c1c9aa477708b
pakjiyeong <78333687+pakjiyeong@users.noreply.github.com>
2021-02-23 16:42:25 +0900

    v.2.20210223
    again

--- js/script.js
+++ js/script.js
@@ -16,31 +16,75 @@
     )
 // ----------------------------------------------------------------SECTION-01(home) AREA END 
 // ----------------------------------------------------------------SECTION-02(STORY) AREA START
-    function numberCounter(target_frame, target_number) {
-    this.count = 0; this.diff = 0;
-    this.target_count = parseInt(target_number);
-    this.target_frame = document.getElementById(target_frame);
-    this.timer = null;
-    this.counter();
-    };
-    numberCounter.prototype.counter = function() {
-    var self = this;
-    this.diff = this.target_count - this.count;
+    // var counter= 296842;
 
-    if(this.diff > 0) {
-        self.count += Math.ceil(this.diff / 10);
-    }
-    this.target_frame.innerHTML = this.count.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+    // $({ val : 0 }).animate({ val : counter }, {
+    // duration: 2000,
+    // step: function() {
+    //     var num = numberWithCommas(Math.floor(this.val));
+    //     $(".memberCountCon").text(num);
+    // },
+    // complete: function() {
+    //     var num = numberWithCommas(Math.floor(this.val));
+    //     $(".memberCountCon").text(num);
+    // }
+    // });
 
-    if(this.count < this.target_count) {
-        this.timer = setTimeout(function() { self.counter(); }, 20);
-    } else {
-        clearTimeout(this.timer);
-    }
-    };
-    new numberCounter("counter1", 2896887);
-    new numberCounter("counter2", 840);
-    new numberCounter("counter3", 700);
+    // function numberWithCommas(x) {
+    //     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    // }
+    // -----------------------------------------------------
+    //         var counter= 2896887;
+  
+    //         $({ val : 0 }).animate({ val : counter }, {
+    //         duration: 2000,
+    //         step: function() {
+    //             var num = numberWithCommas(Math.floor(this.val));
+    //             $(".memberCountCon").text(num);
+    //         },
+    //         complete: function() {
+    //             var num = numberWithCommas(Math.floor(this.val));
+    //             $(".memberCountCon").text(num);
+    //         }
+    //         });
+
+    //         function numberWithCommas(x) {
+    //             return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    //         }
+
+$(function(){
+    var Offset = $('.counterstart').offset();
+    $(window).scroll(function(){
+        if($(window).scrollTop()>Offset.top){
+            function numberCounter(target_frame, target_number) {
+                this.count = 0; this.diff = 0;
+                this.target_count = parseInt(target_number);
+                this.target_frame = document.getElementById(target_frame);
+                this.timer = null;
+                this.counter();
+                };
+                numberCounter.prototype.counter = function() {
+                var self = this;
+                this.diff = this.target_count - this.count;
+            
+                if(this.diff > 0) {
+                    self.count += Math.ceil(this.diff / 10);
+                }
+                this.target_frame.innerHTML = this.count.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+            
+                if(this.count < this.target_count) {
+                    this.timer = setTimeout(function() { self.counter(); }, 20);
+                } else {
+                    clearTimeout(this.timer);
+                }
+                };
+                new numberCounter("counter1", 2896887);
+                new numberCounter("counter2", 840);
+                new numberCounter("counter3", 700);
+        }
+    });
+});
+    
 // ----------------------------------------------------------------SECTION-02(STORY) AREA END 
 
 // ----------------------------------------------------------------SECTION-03(DULLE) AREA START
